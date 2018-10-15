0ccbc936465fda3a6e8d81ac2e5f95acad342bbe
Giuseppe Longo <giuseppe@glongo.it>
2018-10-15 20:16:27 +0200

    device: add runmode id
    To setup the capture method correctly, a device needs to know to which
    runmode it belongs to such that the runmode function, of the capture method,
    will get the proper device.

--- src/source-nfq.c
+++ src/source-nfq.c
@@ -877,10 +877,11 @@ int NFQRegisterQueue(const uint16_t number)
     nq->queue_num = number;
     receive_queue_num++;
     SCMutexUnlock(&nfq_init_lock);
+
     snprintf(queue, sizeof(queue) - 1, "NFQ#%hu", number);
     LiveRegisterDevice(queue);
 
-    ntv->livedev = LiveGetDevice(queue);
+    ntv->livedev = LiveGetDevice(queue, RUNMODE_NFQ);
 
     if (ntv->livedev == NULL) {
         SCLogError(SC_ERR_INVALID_VALUE, "Unable to find Live device");
