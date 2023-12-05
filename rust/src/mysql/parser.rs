de1ed89f2988931fc0f87684970731d6c2cea702
Giuseppe Longo <giuseppe@glongo.it>
2023-12-05 15:14:06 +0100

    fix

--- rust/src/mysql/parser.rs
+++ rust/src/mysql/parser.rs
@@ -1,6 +1,10 @@
-use nom::*;
-use nom::{crlf, IResult};
+use nom7::bytes::complete::{take, take_till};
+use nom7::combinator::{cond, map, verify};
+use nom7::number::complete::{be_u16, be_u24, be_u8, le_u16, le_u32};
+use nom7::IResult;
 use std;
+
+const CLIENT_PLUGIN_AUTH: u64 = 0x8000;
 
 #[derive(Debug)]
 pub struct MysqlPacket {
@@ -9,75 +13,76 @@ pub struct MysqlPacket {
 }
 
 #[derive(Debug)]
-pub struct MysqlHandshakeRequest {
+pub struct MysqlHandshakeRequest<'a> {
     pub header: MysqlPacket,
     pub protocol: u8,
-    pub version: String,
+    pub version: &'a [u8],
     pub conn_id: u32,
-    pub salt1: String,
-    pub capability_flag1: u16,
+    pub salt1: &'a [u8],
+    pub capability_flags1: u16,
     pub character_set: u8,
     pub status_flags: u16,
     pub capability_flags2: u16,
     pub auth_plugin_len: u8,
-    pub salt2: String,
-    pub auth_plugin_data: Option<String>
+    pub salt2: &'a [u8],
+    pub auth_plugin_data: Option<&'a [u8]>,
 }
 
-pub fn mysql_parse_handshake(i: &[u8]) -> IResult<&[u8], MysqlHandshakeRequest> {
+#[derive(Debug)]
+pub struct MysqlHandshakeResponse<'a> {
+    pub header: MysqlPacket,
+    pub client_flags: u32,
+    pub max_packet_size: u32,
+    pub character_set: u8,
+    pub username: &'a [u8],
+    pub auth_response: &'a [u8],
+    pub database: &'a [u8],
+    pub client_plugin_name: &'a [u8],
+}
+
+pub fn mysql_parse_handshake<'a>(i: &'a [u8]) -> IResult<&'a [u8], MysqlHandshakeRequest<'a>> {
     let (i, pkt_len) = be_u24(i)?;
     let (i, pkt_num) = be_u8(i)?;
-    let (i, protocol) = verify(be_u8, |x| x == 0x0a as u8)(i)?;
-    let (i, version) = map(take_till(|ch| ch == 0x00), |s: &[u8]| String::from_utf8(s.to_vec()).unwrap())(i)?;
-    let (i, _) = take(1)(i)?;
+    let (i, protocol) = verify(be_u8, |x| *x == 0x0a as u8)(i)?;
+    let (i, version) = take_till(|ch| ch == 0x00)(i)?;
+    let (i, _) = take(1_usize)(i)?;
     let (i, conn_id) = le_u32(i)?;
-    let (i, salt1) = map(take_str(8), |s: &str| s.into())(i)?;
-    let (i, _) = take(1)(i)?;
+    let (i, salt1) = take(8_usize)(i)?;
+    let (i, _) = take(1_usize)(i)?;
     let (i, capability_flags1) = le_u16(i)?;
     let (i, character_set) = be_u8(i)?;
     let (i, status_flags) = le_u16(i)?;
     let (i, capability_flags2) = be_u16(i)?;
-    
+    let (i, auth_plugin_len) = be_u8(i)?;
+    let (i, _) = take(10_usize)(i)?;
+    let (i, salt2) = take_till(|ch| ch == 0x00)(i)?;
+    let (i, auth_plugin_data) = cond(auth_plugin_len > 0, take_till(|ch| ch == 0x00))(i)?;
+
+    Ok((
+        i,
+        MysqlHandshakeRequest {
+            header: MysqlPacket { pkt_len, pkt_num },
+            protocol: 0x0a,
+            version,
+            conn_id,
+            salt1,
+            capability_flags1,
+            character_set,
+            status_flags,
+            capability_flags2,
+            auth_plugin_len,
+            salt2,
+            auth_plugin_data,
+        },
+    ))
 }
 
-named!(pub mysql_parse_handshake<&[u8], MysqlHandshakeRequest>,
-    do_parse!(
-        pkt_len: map!(take!(3), |len: &[u8]| read_u24(len)) >>
-        pkt_num: be_u8 >>
-        //protocol: be_u8 >>
-        protocol: verify!(be_u8, |x| x == 0x0a as u8) >>
-        version: map!(take_till!(|ch| ch == 0x00), |s: &[u8]| String::from_utf8(s.to_vec()).unwrap()) >>
-        nil: take!(1) >>
-        conn_id: le_u32 >>
-        salt1: map!(take_str!(8), |s: &str| s.into()) >>
-        nil: take!(1) >>
-        capability_flag1: le_u16 >>
-        character_set: be_u8 >>
-        status_flags: le_u16 >>
-        capability_flags2: be_u16 >>
-        auth_plugin_len: be_u8 >>
-        reversed: take!(10) >>
-        salt2: map!(take_till!(|ch| ch == 0x00), |s: &[u8]| String::from_utf8(s.to_vec()).unwrap()) >>
-        auth_plugin_data: cond!(auth_plugin_len > 0, map!(take_till!(|ch| ch == 0x00), |s: &[u8]| String::from_utf8(s.to_vec()).unwrap()))
-        >>
-        (
-            MysqlHandshakeRequest {
-                header: MysqlPacket {
-                    pkt_len,
-                    pkt_num,
-                },
-                protocol: 0x0a,
-                version,
-                conn_id,
-                salt1,
-                capability_flag1,
-                character_set,
-                status_flags,
-                capability_flags2,
-                auth_plugin_len,
-                salt2,
-                auth_plugin_data
-            }
-        )
-    )
-);
+pub fn mysql_parse_handshake_response<'a>(i: &'a [u8]) -> IResult<&'a [u8], MysqlHandshakeResponse<'a>> {
+    let (i, pkt_len) = be_u24(i)?;
+    let (i, pkt_num) = be_u8(i)?;
+    let (i, client_flags) = be_u32(i)?;
+    let (i, max_packet_size) = be_u32(i)?;
+    let (i, character_set) = be_u8(i)?;
+    let (i, _) = take(23)(i)?;
+    let (i, username) = take_till(|ch| ch == 0x00)(i)?;
+}
